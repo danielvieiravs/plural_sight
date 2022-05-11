4464441cabde6d652240c9778c6f2d3e9adf5132
danielvieiravs <danielvieiravs@live.com.pt>
2022-05-11 08:28:42 +0100

    async functions

--- javascript_core_language/promises_and_async/src/creating.mjs
+++ javascript_core_language/promises_and_async/src/creating.mjs
@@ -61,6 +61,15 @@ export function xhr(){
 }
 
 export function allPromises(){
+    /*
+    The Promise.all() method takes an iterable of promises as an input, and
+    returns a single Promise that resolves to an array of the results of
+    the input promises. This returned promise will resolve when all of the
+    input's promises have resolved, or if the input iterable contains no promises.
+    It rejects immediately upon any of the input promises rejecting or non-promises
+    throwing an error, and will reject with this first rejection message / error.
+    */
+
     let categories = axios.get("http://localhost:3000/itemCategories");
     let statuses = axios.get("http://localhost:3000/orderStatuses");
     let userTypes = axios.get("http://localhost:3000/userTypes");
@@ -80,6 +89,20 @@ export function allPromises(){
 }
 
 export function allSettled(){
+    /*
+    The Promise.allSettled() method returns a promise that resolves after all
+    of the given promises have either fulfilled or rejected, with an array of objects
+    that each describes the outcome of each promise.
+
+    It is typically used when you have multiple asynchronous tasks that are not
+    dependent on one another to complete successfully, or you'd always like to
+    know the result of each promise.
+
+    In comparison, the Promise returned by Promise.all() may be more appropriate
+    if the tasks are dependent on each other / if you'd like to immediately reject
+    upon any of them rejecting.
+    */
+
     let categories = axios.get("http://localhost:3000/itemCategories");
     let statuses = axios.get("http://localhost:3000/orderStatuses");
     let userTypes = axios.get("http://localhost:3000/userTypes");
@@ -89,10 +112,10 @@ export function allSettled(){
         .then((values) => {
             let results = values.map(v => {
                 if (v.status === "fulfilled") {
-                    return `FULFILLED: ${JSON.stringify(v.value.data[0])}`;
+                    return `FULFILLED: ${JSON.stringify(v.value.data[0])} `;
                 }
 
-                return `REJECTED: ${v.reason.message}`;
+                return `REJECTED: ${v.reason.message} `;
             })
 
             setText(results);
@@ -102,4 +125,16 @@ export function allSettled(){
 }
 
 export function race(){
+    /*
+    The Promise.race() method returns a promise that fulfills or rejects as soon
+    as one of the promises in an iterable fulfills or rejects, with the value or
+    reason from that promise.
+    */
+
+    let users = axios.get("http://localhost:3000/users");
+    let backup = axios.get("http://localhost:3000/users");
+
+    Promise.race([users, backup])
+        .then(users => setText(JSON.stringify(users.data)))
+        .catch(reason => setText(reason));
 }
